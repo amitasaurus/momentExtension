50d037874b9875ca9b06046a77b3c7c2355d21f2
Amit Singh <amit@coinswitch.co>
2019-08-29 12:05:17 +0530

    Changes

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 (function() {
-	angular.module('myApp', ['ngAnimate', '720kb.fx']).controller('mainCtrl', function($scope, $http, $timeout) {
-		function getTime(){
-			console.log('Getting time');
-			function n(n){
-			    return n > 9 ? "" + n: "0" + n;
-			}
-			var d = new Date();
-			$scope.hour = d.getHours();
-			$scope.min = n(d.getMinutes());
-			$scope.ampm = $scope.hour >= 12 ? 'pm' : 'am';
-		}
-		getTime();
-		var timer = function(){
-			getTime();
-			$timeout(timer,60000);
-		}
-		$timeout(timer,60000);
-		$http.get('https://favqs.com/api/qotd').then(function(response) {
-			console.log(response);
-			$scope.quote = response.data.quote.body;
-			$scope.author = response.data.quote.author;
-		})
-	})
+    angular
+        .module("myApp", ["ngAnimate", "720kb.fx"])
+        .controller("mainCtrl", function($scope, $http, $timeout) {
+            function getTime() {
+                console.log("Getting time");
+                function n(n) {
+                    return n > 9 ? "" + n : "0" + n;
+                }
+                var d = new Date();
+                $scope.hour = d.getHours();
+                $scope.min = n(d.getMinutes());
+                $scope.ampm = $scope.hour >= 12 ? "pm" : "am";
+            }
+            getTime(); //Changes
+            var timer = function() {
+                getTime();
+                $timeout(timer, 60000);
+            };
+            $timeout(timer, 60000);
+            $http.get("https://favqs.com/api/qotd").then(function(response) {
+                console.log(response);
+                $scope.quote = response.data.quote.body;
+                $scope.author = response.data.quote.author;
+            });
+        });
 })();
 /**
 Weather
